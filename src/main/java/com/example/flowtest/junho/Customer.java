ad3ec356ce05445286ee2fcb11f57d00f7ddbffe
slvyblmn <slvyblmm@gmail.com>
2026-07-10 16:36:57 +0900

    커스터머 엔티티 생성

---
+++ src/main/java/com/example/flowtest/junho/Customer.java
@@ -0,0 +1,15 @@
+package com.example.flowtest.junho;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name= "custemers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
