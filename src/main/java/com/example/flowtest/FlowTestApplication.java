41d451a5874bead7921849ea9c8d39f9d5b95368
slvyblmn <slvyblmm@gmail.com>
2026-07-10 16:11:02 +0900

    init commit

---
+++ src/main/java/com/example/flowtest/FlowTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/flowtest/FlowTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
